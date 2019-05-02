d5ef101daa6a50effd81ce46abfc9d0a6bc0dca7
landoncrawford <landoncr4d@gmail.com>
2019-05-02 18:56:42 -0400

    week5

---
+++ Homework 5 Landon Crawford/Assets/PlayerDetector.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDetector : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Ray playerRay = new Ray(transform.position, Vector3.right);
+        RaycastHit hit;
+
+        if (Physics.Raycast(playerRay, out hit, 5))
+        {
+            print(hit.transform.gameObject);
+        }
+        else
+        {
+            print("nothing ahead");
+        }
+    }
+}
